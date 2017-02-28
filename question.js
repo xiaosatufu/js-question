1330a00c109931a33ff2c3fa2fb3789a9097cb21
xiaosatufu <xiaosatufu@163.com>
2017-02-28 10:49:19 +0800

    jquery ajax post 详解

--- question.js
+++ question.js
@@ -8,3 +8,36 @@ var audio  = document.getElementById('J_BgAudio');
 document.addEventListener('touchstart', function(){
     audio.play();
 }, false);
+
+
+
+// jquery ajax POST 例子详解
+    function test(){
+   $.ajax({
+            //提交数据的类型 POST GET
+            type:"POST",
+            //提交的网址
+            url:"testLogin.aspx",
+            //提交的数据
+            data:{Name:"sanmao",Password:"sanmaoword"},
+            //返回数据的格式
+            datatype: "html",//"xml", "html", "script", "json", "jsonp", "text".
+            //在请求之前调用的函数
+            beforeSend:function(){$("#msg").html("logining");},
+            //成功返回之后调用的函数
+            success:function(data){
+           $("#msg").html(decodeURI(data));
+            }   ,
+            //调用执行后调用的函数
+            complete: function(XMLHttpRequest, textStatus){
+               alert(XMLHttpRequest.responseText);
+               alert(textStatus);
+                //HideLoading();
+            },
+            //调用出错执行的函数
+            error: function(){
+                //请求出错处理
+            }
+         });
+
+  }
